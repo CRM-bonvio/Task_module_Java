f0311a677e2a5915354b03a44751223b924d6c01
igorjan94 <igorjan94@mail.ru>
2015-12-18 13:18:35 +0000

    generators cycle and usage

--- src/main/java/generateFull.sh
+++ src/main/java/generateFull.sh
@@ -1,22 +1,34 @@
+function usage {
+   echo "usage: ./generate.sh <-o directory> entity1 entity2 ..."
+   exit
+}
+
 directory="."
 
 while getopts "o:" opt; do
   case $opt in
     o) directory="$OPTARG";;
+    \?) usage;;
   esac
 done
 
 shift $(($OPTIND - 1))
-entity=$1
+if [ $# -eq 0 ]
+	then usage
+fi
 
-./generate.sh -c -o $directory/controller/implementations $entity
-./generate.sh -d -o $directory/dao/implementations $entity
-./generate.sh -d -i -o $directory/dao/interfaces $entity
-./generate.sh -s -o $directory/service/implementations $entity
-./generate.sh -s -i -o $directory/service/interfaces $entity
-echo " controller  implementations  $entity  generated"
-echo "    dao      implementations  $entity  generated"
-echo "    dao        interfaces     $entity  generated"
-echo "  service    implementations  $entity  generated"
-echo "  service      interfaces     $entity  generated"
+while [ "$2" != "" ]; do
+  entity=$1
+  ./generate.sh -c -o $directory/controller/implementations $entity
+  ./generate.sh -d -o $directory/dao/implementations $entity
+  ./generate.sh -d -i -o $directory/dao/interfaces $entity
+  ./generate.sh -s -o $directory/service/implementations $entity
+  ./generate.sh -s -i -o $directory/service/interfaces $entity
+  echo " controller  implementations  $entity  generated"
+  echo "    dao      implementations  $entity  generated"
+  echo "    dao        interfaces     $entity  generated"
+  echo "  service    implementations  $entity  generated"
+  echo "  service      interfaces     $entity  generated"
+  shift
+done
 
